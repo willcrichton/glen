a769fd1d96900b14e99c0da786b1884a2fd8c4b6
Will Crichton <crichton.will@gmail.com>
2012-08-05 20:38:29 -0500

    Simplified render function and added Physijs support

--- src/render.js
+++ src/render.js
@@ -1,18 +1,10 @@
-// Called every frame to animate the world and request a new animation frame
-Glen.animateWorld = function( world ){
-	
-	requestAnimationFrame( function(){
-		Glen.animateWorld( world );
-	});
-	Glen.renderWorld( world );
-
-}
-
-// Also called every frame to actually render the world and call any custom rendering from the world object
+var _controlClock = new THREE.Clock();
+// Called every frame to render the world and call any custom rendering from the world object
 Glen.renderWorld = function( world ){
-
-	world.callHook( 'render', world );
-	world.renderer.render(world.scene,world.camera);
 	world.thinkInternal();
-	
+	world.scene.simulate( undefined, 2 );
+	world.controls.update(_controlClock.getDelta());
+	world.callHook( 'render', world );
+	world.renderer.render(world.scene, world.camera);
+	requestAnimationFrame(function(){ Glen.renderWorld( world ); });
 }
