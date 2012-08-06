8d85900c8ea7d25eac073a054bc076113258e802
Will Crichton <crichton.will@gmail.com>
2012-08-05 20:38:02 -0500

    Updating materials to work with Physijs

--- src/material.js
+++ src/material.js
@@ -1,6 +1,15 @@
-Glen.loadTexture = function( path, repeat, opts ) {
-
-	var image = new Image();
+Glen.Material = function( path, args ) {
+	args = args || {};
+	var material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( path ) }),
+		args.friction || .8, 
+		args.restitution || .4
+	);
+	material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+	material.map.repeat.set( args.repeatX || 2.5, args.repeatY || 2.5 );
+	return material;
+	
+	/*var image = new Image();
 	image.onload = function () { 
 		texture.needsUpdate = true;
 		texture.repeat.x = repeat[0] / image.width * 16;
@@ -10,8 +19,7 @@ Glen.loadTexture = function( path, repeat, opts ) {
 
 	var texture  = new THREE.Texture( image, new THREE.UVMapping(), THREE.RepeatWrapping, THREE.RepeatWrapping, THREE.NearestFilter, THREE.LinearMipMapLinearFilter );
 	
-	return new THREE.MeshLambertMaterial( $.merge({ map: texture },opts || {}) );
-
+	return new THREE.MeshLambertMaterial( $.merge({ map: texture },opts || {}) );*/
 }
 
 
