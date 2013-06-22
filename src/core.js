99b9f52a91a6d6c09d5007d09a82124e672280ca
Will Crichton <willcrichton@cmu.edu>
2013-06-22 12:39:00 -0400

    Cut down the core.js for now

--- src/core.js
+++ src/core.js
@@ -1,10 +1,8 @@
 // presets for other scripts
-var Glen;
+var Glen = {};
 (function(){
     'use strict';
-    
+
     Physijs.scripts.worker = 'physijs_worker.js';
     Physijs.scripts.ammo = 'ammo.js';
-    
-    Glen = {};
 })();
