de70b031ca4955626c24d389813f924dbf6b976e
Will Crichton <willcrichton@cmu.edu>
2013-06-22 12:37:45 -0400

    Replaced ridiculous entity system with regular JS classes

--- src/entity.js
+++ src/entity.js
@@ -6,7 +6,7 @@
 (function(){
     'use strict';
 
-    Glen._entities = {};
+    Glen.Entity = {};
 
     // copies properties of b onto a, like jQuery.extend
     function _extend(a, b){
@@ -16,207 +16,128 @@
         return a;
     }
 
-    /* Create a custom entity
-     * name: unique string associated with entity (don't overrwrite existing entities!)
-     * construct: constructor for the entity, called after all extender constructors
-     * extender: name of entity to extend, if any
-     * extendArgs: map of arguments to pass to extended entity
-     * Note for clarity: constructors bubble up from the bottom-level extenders, whereas arguments are 
-     * constructed top-down (e.g. top-level arguments overrwrite low-level ones).
-     */
-    Glen.registerEntity = function(name, construct, extender, extendArgs){
-        if (typeof Glen._entities[name] != "undefined") {
-            throw new Error('"' + name + '" is already defined, cannot reigster it');
+    _extend(
+        THREE.Object3D.prototype,
+        {
+            getPosition: function(){
+                return this.position.clone();
+            },
+            
+            setPosition: function( vector ){
+                this.position = vector.clone();
+            },
+            
+            getRotation: function(){
+                return this.rotation.clone();
+            },
+            
+            setRotation: function( vector ){
+                this.rotation = vector.clone();
+            },
+            
+            rotate: function( vector ){
+                this.setRotation( this.getRotation().add(vector) );
+            },
+            
+            getMaterial: function(){
+                return this.materials[0];
+            },
+            
+            setMaterial: function(material){
+                this.materials[0] = material;
+            },
+
+            addHook: function(hook, callback){
+                if (!this.hooks) {
+                    this.hooks = {}
+                }
+                if (!this.hooks[hook]) { 
+                    this.hooks[hook] = [];
+                }
+                this.hooks[hook].push(callback);
+            },
+            
+            callHook: function( hook ){
+                if (!this.hooks || !this.hooks[hook]) {
+                    return;
+                }
+                var args = Array.prototype.slice.call(arguments);
+                args.splice( 0, 1 );
+                for (var i in this.hooks[hook]) {
+                    this.hooks[hook][i].apply(this, args);
+                }
+            },
+            
+            remove: function(){
+                Glen._world.remove( this );
+            },
         }
-        Glen._entities[name] = {
-            construct: construct,
-            extender: extender,
-            extendArgs: extendArgs
-        };
-    };
+    );
 
-    // Constructor for all entities, takes name and arguments
-    Glen.Entity = function(ent, args){
-        this.hooks = {};
-        this.entType = ent;
-        var entInfo = Glen._entities[this.entType];
-        var entObj;
-        var entArgs = args;
-        var queue = [];
-        do {
-            if(entInfo.construct) queue.unshift(entInfo.construct);
-            entArgs = _extend(entArgs, 
-                              typeof entInfo.extendArgs == "function" ?
-                              entInfo.extendArgs(entArgs) :
-                              entInfo.extendArgs);
-            entInfo = Glen._entities[entInfo.extender];
-        } while(typeof entInfo != "undefined");
-        for(var i = 0; i < queue.length; i++){
-            var newObj = queue[i].call(entObj, entArgs);
-            if(newObj !== undefined) entObj = newObj;
-        }
-        _extend(entObj, this);
-
-        Glen._world.addEntity(entObj);
-        return entObj;
-    };
-
-    Glen.Entity.constructor = Glen.Entity;
-    Glen.Entity.prototype = new THREE.Object3D();
-    _extend(Glen.Entity.prototype,
-            {
-                getType: function(){
-                    return this.entType;
-                },
-                
-                getPos: function(){
-                    return this.getMesh().position.clone();
-                },
-                
-                setPos: function( vector ){
-                    this.getMesh().position = vector.clone();
-                },
-                
-                getRotation: function(){
-                    return this.getMesh().rotation.clone();
-                },
-                
-                setRotation: function( vector ){
-                    this.getMesh().rotation = vector.clone();
-                },
-                
-                rotate: function( vector ){
-                    this.setRotation( this.getRotation().add(vector) );
-                },
-                
-                getMaterial: function(){
-                    return this.getMesh().materials[0];
-                },
-                
-                setMaterial: function(material){
-                    this.getMesh().materials[0] = material;
-                },
-
-                addHook: function(hook, callback){
-                    if(!this.hooks[hook]) this.hooks[hook] = [];
-                    this.hooks[hook].push(callback);
-                },
-                
-                callHook: function( hook ){
-                    var args = Array.prototype.slice.call(arguments);
-                    args.splice( 0, 1 );
-                    if(this.hooks[hook]) {
-                        for (var i in this.hooks[hook]) {
-                            this.hooks[hook][i].apply(this, args);
-                        }
-                    }
-                },
-                
-                remove: function(){
-                    Glen._world.removeEntity( this );
-                },
-            });
-
-    /* BASIC GEOMETRIES */
-    Glen.registerEntity("basic", function(args){
+    function getMaterial(args) {
         var material;
         if(args.material){
             material = args.material;
         } else if(args.color){
-            material = Glen.ColorMaterial(args.color);
+            material = Glen.Util.ColorMaterial(args.color);
         } else {
-            material = new Glen.ColorMaterial(0xffffff);
+            material = new Glen.Util.ColorMaterial(0xffffff);
         }
-        if(!args.meshfn) throw new Error("Basic entity type requires Physijs mesh constructor");
-        if(!args.geometry) throw new Error("Basic entity type requires some THREE.Geometry type");
-        var mesh = new args.meshfn(args.geometry, material, typeof args.mass == "undefined" ? 10 : args.mass);
-        if(args.rotation) mesh.rotation.copy(args.rotation);
-        mesh.position.copy(args.position || Glen.Vector(0,0,0));
+        return material;
+    }
+
+    function setLocation(mesh, args) {
+        mesh.rotation.copy(args.rotation || Glen.Util.Vector(0,0,0));
+        mesh.position.copy(args.position || Glen.Util.Vector(0,0,0));
         mesh.castShadow = true;
+    }
 
-        return mesh;
-    });
-
-    Glen.registerEntity(
-        "block", undefined, "basic", 
-        function(args){
-            return {
-                geometry: new THREE.CubeGeometry(args.width, args.height, args.depth),
-                meshfn: Physijs.BoxMesh
-            };
+    Glen.Entity.Block = function(args) {
+        var geometry = new THREE.CubeGeometry(args.width, args.height, args.depth);
+        Physijs.BoxMesh.call(this, geometry, getMaterial(args), args.mass, {
+            restitution: args.restitution || .2
         });
+        setLocation(this, args);
+    }
+    Glen.Entity.Block.prototype = Object.create(Physijs.BoxMesh.prototype);
 
-    Glen.registerEntity("floor", function(){
+    Glen.Entity.Floor = function(args) {
+        args.height = 1;
+        args.mass = 0;
+        Glen.Entity.Block.call(this, args)
         this.castShadow = false;
         this.receiveShadow = true;
-    }, "block", {height: 1, mass: 0});
+    }
+    Glen.Entity.Floor.prototype = Object.create(Glen.Entity.Block.prototype);
 
-    Glen.registerEntity(
-        "sphere", undefined, "basic", 
-        function(args){
-            return {
-                geometry : new THREE.SphereGeometry(args.radius, args.segments, args.rings),
-                meshfn: Physijs.SphereMesh
-            };
+    Glen.Entity.Sphere = function(args) {
+        var geometry = new THREE.SphereGeometry(args.radius, args.segments, args.rings);
+        Physijs.SphereMesh.call(this, getMaterial(args), 0, {
+            restitution: args.restitution || .2
         });
+        setLocation(this, args);
+    }
+    Glen.Entity.Sphere.prototype = Object.create(Physijs.SphereMesh.prototype);
 
-    Glen.registerEntity(
-        "text", undefined, "basic", 
-        function(args){
-            return {
-                geometry: new THREE.TextGeometry(args.text, args),
-                meshfn: Physijs.ConvexMesh
-            };
+    Glen.Entity.Text = function(args) {
+        var geometry = new THREE.TextGeometry(args.text, args);
+        Physijs.ConvexMesh.call(this, getMaterial(args), 0, {
+            restitution: args.restitution || .2
         });
+        setLocation(this, args);
+    }
+    Glen.Entity.Text.prototype = Object.create(Physijs.ConvexMesh.prototype);
 
-
-    /* LIGHTING */
-    Glen.registerEntity("pointLight", function(args){
-        var light = new THREE.PointLight(args.color || 0xFFFFFF);
-        light.position.copy(args.position || Glen.Vector(0, 0, 0));
-        light.intensity = args.intensity;
-        return light;
-    });
-
-    Glen.registerEntity("directionalLight", function(args){
-        var light = new THREE.DirectionalLight(args.color || 0xFFFFFF, args.intensity, args.distance);
-        light.position.copy(args.position || Glen.Vector(0, 0, 0));
-        light.target.position.copy(args.target || Glen._world.scene.position);
-        light.castShadow = true;
-        // what is this I don't even
-        // TODO: figure out how lighting works
-        light.shadowCameraLeft = -60;
-        light.shadowCameraTop = -60;
-        light.shadowCameraRight = 60;
-        light.shadowCameraBottom = 60;
-        light.shadowCameraNear = 20;
-        light.shadowCameraFar = 200;
-        light.shadowBias = -0.0001;
-        light.shadowMapWidth = light.shadowMapHeight = 2048;
-        light.shadowDarkness = 0.7;
-        return light;
-    });
-
-    Glen.registerEntity("ambientLight", function(args){
-        return new THREE.AmbientLight(args.color || 0xFFFFFF);
-    });
-
-
-    /* OTHER */
-    Glen.registerEntity("model", function(args){
-        if(typeof args.geometry == "undefined"){
+    Glen.Entity.Model = function(args) {
+        if(args.geometry === undefined) {
             var loader = new THREE.JSONLoader();
             var ent = this;
-            // hack around entity system to make asynchronous loading more convenient
-            // TODO: integrate this with entities (or leave it exclusively to user)
             loader.load({model: args.model, callback: function(geometry){
-                var mesh = new Physijs.ConvexMesh(geometry, new THREE.MeshLambertMaterial());
-                ent.setMesh(mesh);
-                world.scene.Object(mesh);
+                Physijs.ConvexMesh.call(ent, geometry, new THREE.MeshLambertMaterial());
             }});
-            return undefined;
         } else {
-            return new Physijs.ConvexMesh(args.geometry, new THREE.MeshLambertMaterial());
+            Physijs.ConvexMesh.call(this, args.geometry, new THREE.MeshLambertMaterial());
         }
-    });
+    }
+    Glen.Entity.Model.prototype = Object.create(Physijs.ConvexMesh.prototype);
 })();
